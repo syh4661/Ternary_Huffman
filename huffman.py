25509b24857015a1eaf0f7cd613fce5633e5743a
YongHyeok Seo <syh4661@Ses-Mac-mini-2.local>
2018-06-01 20:05:34 +0900

    final

--- huffman.py
+++ huffman.py
@@ -15,10 +15,11 @@ def create_tree(probabilities):
     while p.qsize() > 2:         # 2. While there is more than two node
         l, m, r = p.get(), p.get(), p.get()  # 2a. remove three highest nodes
         node = HuffmanNode(l, m, r) # 2b. create internal node with children
-        p.put((l[0]+m[0]+r[0], node)) # 2c. add new node to queue 
-        
+        p.put((l[0]+m[0]+r[0], node)) # 2c. add new node to queue
+    
     return p.get()               # 3. tree is complete - return root node
-
+1
+7
 # Recursively walk the tree down to the leaves,
 #   assigning a code value to each symbol
 def walk_tree(node, prefix="", code={}):
@@ -30,16 +31,27 @@ def walk_tree(node, prefix="", code={}):
         walk_tree(n.mid, prefix + "1")
         walk_tree(n.right, prefix + "0")
     return(code)
-
+mode = int(input('Enter the mode 1 to top 2 to bottom :'))
 size = int(input('Enter the number of your codes :'))
+
 
 probability = [] # create empty list
 
 for i in range(0, size): # set up loop to run 5 times
     print('Please enter the',i+1,'input')
-    number = str(input('The signal number:              ')) # prompt user for number 
-    prob = float(input('The probability:                '))
+    number = str(input('The signal number:              ')) # prompt user for number
+    prob2 = float(input('The probability:                '))
+    print(prob2)
+    if (prob2==0.0)and(mode==1):
+        prob = 0.001
+    else :
+        if (prob2==0.0)and(mode ==2):
+            prob = -0.001
+        else :
+            if (prob2 != 0.0):
+                prob = prob2
     tup=(prob,number)
+    print(tup)
     probability.append(tup) # append to our_list
 node = create_tree(probability)
 
@@ -53,7 +65,7 @@ code = walk_tree(node)
 for i in sorted(probability, reverse=True):
     print ('{:6.3f}'.format(i[0]), code[i[1]])
     leng_sum+=i[0]*len(code[i[1]])
-for i in sorted(probability, reverse=True):    
+for i in sorted(probability, reverse=True):
     var_sum+=i[0]*((len(code[i[1]])-leng_sum)**2)
 print('|----------------MEASURES-----------------------|')
 print(' The expected Length : ','{:6.2f}'.format(leng_sum))
